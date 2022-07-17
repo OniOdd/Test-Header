045a9bc36306000ff5b2ce5cd7dcb16da02640ad
OniOdd <103828218+OniOdd@users.noreply.github.com>
2022-07-17 17:45:17 +0300

    Just test

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Burger Menu
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu');
+if (iconMenu) {
+	iconMenu.addEventListener('click', function () {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
